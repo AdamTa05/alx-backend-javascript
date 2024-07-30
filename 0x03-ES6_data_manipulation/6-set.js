12e4d08c874e32ae3d08c2f698cf0ea53f229b41
AdamTa05 <adamtassaft05@gmail..com>
2024-07-30 23:47:32 +0000

    done

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,4 +1,4 @@
-iexport default function setFromArray(array) {
+export default function setFromArray(array) {
   return new Set(array);
 }export default function setFromArray(array) {
   return new Set(array);
